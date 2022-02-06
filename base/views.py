768ea29d8b4b9cf95694db82112577c1d4707604
DHEERAJ KUMAR <dheerajrao20@outlook.com>
2022-02-06 16:25:17 +0530

    updating code

--- base/views.py
+++ base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def lobby(request):
+    return render(request, 'lobby.html')
 
 def room(request):
     return render(request, 'room.html')
-
+    
+def index(request):
+    return render(request, 'index.html')
